57cc07383ca69eb8038824bb7430aca55029dca1
JiwooPark <jpark830@me.com>
2016-08-03 17:58:47 -0700

    First commit

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,63 @@
+class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+
+
+  end
+  def show
+    @contact = Contact.find(params[:id])
+
+    rescue ActiveRecord::RecordNotFound => e
+    redirect_to contacts_path, :notice => "This entry does not exist!"
+
+  end
+
+  def age(birthday_year)
+
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(user_params)
+
+      if @contact.name.length < 1
+        redirect_to new_contact_path, :notice => "Please fill in the information!"
+      elsif @contact.save
+        redirect_to contacts_path, :notice => "Successfully Created!"
+      else
+        render "new"
+      end
+
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update_attributes(user_params)
+      redirect_to contacts_path, :notice => "Successfully updated!!"
+    else
+      render "edit"
+    end
+  end
+
+
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+
+    redirect_to contacts_path, :notice => "Successfully deleted !!"
+  end
+
+
+  private
+  def user_params
+    params.require(:contact).permit(:name,:birthday,:phone,:position,:sa_yeok,:mok_jang,:sun_kyo,:email,:address_building,:address_city,:address_zip,:address_state,:other_1,:other_2,:other_3)
+  end
+end
---
+++ db/migrate/20160728040424_create_contacts.rb
@@ -0,0 +1,23 @@
+class CreateContacts < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :position
+      t.string :mok_jang
+      t.string :sa_yeok
+      t.string :sun_kyo
+      t.string :address_building
+      t.string :address_city
+      t.string :address_zip
+      t.string :address_state
+      t.string :phone
+      t.string :email
+      t.date :birthday
+      t.string :other_1
+      t.string :other_2
+      t.string :other_3
+
+      t.timestamps null: false
+    end
+  end
+end
