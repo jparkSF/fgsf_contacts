f8af3485d523deae51c39ecddf7628b5f031e0d6
Sam Kim <yonghyun@squareup.com>
2016-10-05 10:09:54 -0700

    refine script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,30 +6,33 @@ env="$1"
 
 case $env in
     test)
-    port=4444
-    ;;
+        port=4444
+        ;;
     prod|production)
-    port=3333
-    ;;
+        port=3333
+        ;;
     *)
-    # unknown option
-    echo "Must specify environemtn (ie: test, prod, or production)"
-    exit 1
-    ;;
+        echo "Must specify environment (ie: test, prod, or production)"
+        exit 1
+        ;;
 esac
 
 if [ -e "$PWD/tmp/pids/server.pid" ]; then
-	echo '******Killing server..'
+	echo '******Killing server'
 	less $PWD/tmp/pids/server.pid | xargs kill
+	echo ''
 fi
 
 echo '******Pulling latest master'
 git pull
+echo ''
 
 echo '******Rake migrating DB'
 bundle exec rake db:migrate
+echo ''
 
 echo '******Running Server as daemon'
 rails s -p $port -d
+echo ''
 
 echo '******DONE'
