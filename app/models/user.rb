533caa269e73301e6451d1fca9fc80bd3dbe4692
jparkSF <jpark830@me.com>
2016-10-05 20:29:03 -0700

    added :timeoutable
    added user timeout after x min of inactivity (5min)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable, :timeout_in => 5.minutes
 
   has_one :user_role, primary_key: :id, foreign_key: :user_id
 
